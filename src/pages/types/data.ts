614eef0594b27ec87da3c5e705feb4ed25aa5782
xenon <demiladeafuape74@gmail.com>
2026-06-11 23:13:27 +0100

    changed xenlog title

--- src/pages/types/data.ts
+++ src/pages/types/data.ts
@@ -28,7 +28,7 @@ const DEPLOYED_IMAGE = "https://images.unsplash.com/photo-1677442136019-21780eca
 export const allProjects: Project[] = [
   {
     id: 'Xenlog404',
-    title: 'Webhook Tester Platform',
+    title: ' Xenlog404',
     description: 'Real-time HTTP payload listener and telemetry dashboard built to capture, inspect, and debug asynchronous server events.',
     image: '/assets/Xenlog.png', // Replace with actual image path
     lightImage: '/assets/Xenlog-light.png',
