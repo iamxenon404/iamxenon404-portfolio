2e36e2462b9192ead0c85b740c252cfd498312c1
xenon <demiladeafuape74@gmail.com>
2026-06-12 04:10:32 +0100

    added xenlog git repo

--- src/pages/types/data.ts
+++ src/pages/types/data.ts
@@ -32,7 +32,7 @@ export const allProjects: Project[] = [
     description: 'Real-time HTTP payload listener and telemetry dashboard built to capture, inspect, and debug asynchronous server events.',
     image: '/assets/Xenlog.png', // Replace with actual image path
     lightImage: '/assets/Xenlog-light.png',
-    github: 'https://github.com/your-username/webhook-tester', // Replace with your actual repo link
+    github: 'https://github.com/iamxenon404/Xenolog404', // Replace with your actual repo link
     link: 'https://webhook-tester.yourdomain.com',               // Replace or remove if not live
     categoryIds: ['JS'],
     tech: ['Next.js', 'Express', 'TypeScript', 'Tailwind CSS'],
